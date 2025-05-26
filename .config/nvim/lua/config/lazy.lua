9f919a74727357e1a0d8df8c579d534af915cbc1
martipares <martiparesmath@gmail.com>
2025-05-26 22:43:57 +0200

    set mod theme to other progrms and reset nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,18 +20,25 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     -- { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { "martipares/LazyVim", import = "lazyvim.plugins" },
-    -- import any extras modules here
+    -- extra included by default
+    -- { import = "lazyvim.plugins.extras.coding.blink" },
+    -- { import = "lazyvim.plugins.extras.coding.snacks_explorer" },
+    -- enabled languages
     -- { import = "lazyvim.plugins.extras.lang.tex" },
+    -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.coding.luasnip" },
-    -- { import = "lazyvim.plugins.extras.util.dot" },
     -- { import = "lazyvim.plugins.extras.coding.yanky" },
     -- { import = "lazyvim.plugins.extras.editor.dial" },
     -- { import = "lazyvim.plugins.extras.editor.fzf" },
     -- { import = "lazyvim.plugins.extras.editor.inc-rename" },
+    -- { import = "lazyvim.plugins.extras.editor.snacks_picker" },
+    -- { import = "lazyvim.plugins.test.core" },
+    -- { import = "lazyvim.plugins.extras.util.dot" },
     -- { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- { import = "lazyvim.plugins.extras.util.project" },
-    -- { import = "lazyvim.plugins.test.core" },
+    -- import animation extras
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    -- { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -5,11 +5,15 @@ return {
     lazy = true,
     -- theme tokyonight.nvim moon, storm, night, day
     opts = {
-      transparent = false,
       style = "moon",
+      -- transparent = false,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      -- },
       -- on_colors = function(colors)
-      --   colors.fg = "#ffffff"
-      --   colors.fg_dark = "#bbbbbb"
+      --   -- colors.fg = "#ffffff"
+      --   -- colors.fg_dark = "#bbbbbb"
       --   -- colors.fg_gutter = "#999999"
       -- end,
     },
